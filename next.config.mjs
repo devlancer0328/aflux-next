bcf4b7dde6cabdbde94aaba4f3930383e61e9c56
devlancer0328 <topdev0328@proton.me>
2024-02-12 19:34:41 +0100

    corect config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    output: 'export',
+  }
+   
+  module.exports = nextConfig
