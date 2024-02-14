fe45a7415a7c5a763f457676eea9fc2570033673
devlancer0328 <topdev0328@proton.me>
2024-02-14 17:42:25 +0100

    styling header

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -5,17 +5,17 @@ import logo from "@/images/aflux-logo.png";
 
 const Header = () => {
   return (
-    <div className="fixed z-10 flex flex-row justify-center items-center w-screen">
-      <div className="flex flex-row justify-between items-center w-[60vw] py-8">
+    <div className="fixed z-10 flex flex-row justify-center items-center w-screen bg-white h-[48px] top-[60px]">
+      <div className="flex flex-row justify-between items-center w-[60vw]">
         <div>
-          <a className="" href="/">
+          <a className="flex bg-white p-4 rounded-lg" href="/">
             <img src={logo.src} alt="logo" width={80} />
           </a>
         </div>
         <div>
           {HEADER_LINKS.map((link) => (
             <Link
-              className="p-4 uppercase text-white hover:text-green-500"
+              className="p-4 uppercase text-green-600 hover:text-green-400"
               href={link.href}
               key={link.key}
             >
