01c2418e68d89421634bc0a908335c9ae8130b29
devlancer0328 <topdev0328@proton.me>
2024-02-14 17:29:41 +0100

    styling fixed header and add a new logo

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -1,21 +1,28 @@
 import React from "react";
 import { HEADER_LINKS } from "@/constants/header-links";
 import Link from "next/link";
+import logo from "@/images/aflux-logo.png";
 
 const Header = () => {
   return (
-    <div className="flex flex-row justify-center items-center">
-      <div>Logo</div>
-      <div>
-        {HEADER_LINKS.map((link) => (
-          <Link
-            className="p-2 uppercase hover:text-green-500"
-            href={link.href}
-            key={link.key}
-          >
-            {link.label}
-          </Link>
-        ))}
+    <div className="fixed z-10 flex flex-row justify-center items-center w-screen">
+      <div className="flex flex-row justify-between items-center w-[60vw] py-8">
+        <div>
+          <a className="" href="/">
+            <img src={logo.src} alt="logo" width={80} />
+          </a>
+        </div>
+        <div>
+          {HEADER_LINKS.map((link) => (
+            <Link
+              className="p-4 uppercase text-white hover:text-green-500"
+              href={link.href}
+              key={link.key}
+            >
+              {link.label}
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   );
