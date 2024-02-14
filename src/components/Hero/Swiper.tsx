6e3ff49bec84646a2b5eb59d20cb7f7c69e88ff9
devlancer0328 <topdev0328@proton.me>
2024-02-14 17:37:15 +0100

    change hero swiper images

--- src/components/Hero/Swiper.tsx
+++ src/components/Hero/Swiper.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Navigation, Pagination } from "swiper/modules";
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 import "swiper/css";
@@ -8,18 +8,26 @@ import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 
 import img01 from "@/images/hero/pexels01.jpg";
+import img02 from "@/images/hero/pexels02.jpg";
+import img03 from "@/images/hero/pexels03.jpg";
 
 export default () => {
   return (
     <Swiper
       className="h-screen"
-      modules={[Navigation, Pagination]}
+      modules={[Navigation, Pagination, Autoplay]}
       spaceBetween={0}
       slidesPerView={1}
       navigation
       pagination={{ clickable: true }}
       onSwiper={(swiper) => console.log(swiper)}
       onSlideChange={() => console.log("slide change")}
+      autoplay={{
+        delay: 3500,
+        disableOnInteraction: true,
+      }}
+      direction="vertical"
+      effect="slide"
     >
       <SwiperSlide>
         <img
@@ -31,21 +39,14 @@ export default () => {
       <SwiperSlide>
         <img
           className="h-screen w-screen object-cover"
-          src={img01.src}
+          src={img02.src}
           alt="img"
         />
       </SwiperSlide>
       <SwiperSlide>
         <img
           className="h-screen w-screen object-cover"
-          src={img01.src}
-          alt="img"
-        />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img
-          className="h-screen w-screen object-cover"
-          src={img01.src}
+          src={img03.src}
           alt="img"
         />
       </SwiperSlide>
