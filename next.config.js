e1594b80938550c35148734877929d32563a3af8
devlancer0328 <topdev0328@proton.me>
2024-02-15 17:24:09 +0100

    add export

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
-/**
- * @type {import('next').NextConfig}
- */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
+  output: 'export',
   }
-   
+  
   module.exports = nextConfig
