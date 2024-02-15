338be087e65f7ed57ec950be7a8b2ac36a01f681
devlancer0328 <topdev0328@proton.me>
2024-02-15 16:20:37 +0100

    install and add scss

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
-/**
- * @type {import('next').NextConfig}
- */
-const nextConfig = {
-    output: 'export',
-  }
-   
-  module.exports = nextConfig
+// next.config.js
+const path = require('path');
+
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+};
