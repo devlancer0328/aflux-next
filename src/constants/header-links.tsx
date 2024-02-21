0cdd566b03cf68b87231adc797c41c2f8b42d75c
devlancer0328 <topdev0328@proton.me>
2024-02-21 18:13:52 +0100

    change aflux info label name

--- src/constants/header-links.tsx
+++ src/constants/header-links.tsx
@@ -1,6 +1,6 @@
 export const HEADER_LINKS = [
   { href: "#start", key: "start", label: "Start" },
-  { href: "#aflux-info", key: "aflux-info", label: "Czym jest Aflux" },
+  { href: "#aflux-info", key: "aflux-info", label: "Czym jest Aflux?" },
   { href: "#products", key: "products", label: "Produkty" },
   { href: "#where-to-buy", key: "where-to-buy", label: "Gdzie kupić" },
   { href: "#gallery", key: "gallery", label: "Galeria" },
