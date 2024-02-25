4fac9023bed1ab16b17ae65c5779c697a217e9fe
devlancer0328 <topdev0328@proton.me>
2024-02-25 17:07:21 +0100

    add AfluxInfo component with styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import AfluxInfo from "@/components/AfluxInfo/AfluxInfo";
+
 export default function Home() {
-  return <main className="flex min-h-screen"></main>;
+  return (
+    <main className="flex mx-auto justify-center">
+      <AfluxInfo />
+    </main>
+  );
 }
---
+++ src/components/AfluxInfo/AfluxInfo.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import hands_img from "@/images/aflux_info/hands.jpg";
+
+const AfluxInfo = () => {
+  return (
+    <div className="flex bg-green-50 w-full justify-center">
+      <div className="flex flex-row container mx-xl py-32 justify-center items-center">
+        <div className="flex flex-col">
+          <img
+            className="flex rounded-full"
+            src={hands_img.src}
+            alt="hands img"
+            width={540}
+            height={540}
+          />
+        </div>
+        <div className="flex flex-col ml-16">
+          <h1 className="text-5xl mb-4">Aflux®</h1>
+          <h2 className="text-2xl">
+            W czym nasz produkt jest lepszy od innych?
+          </h2>
+          <ul className="my-4 pl-4">
+            <li
+              className="relative my-2 before:inline-block before:w-3 before:h-3 before:mr-4
+            before:bg-green-400 before:rounded-full before:border before:border-green-600"
+            >
+              Bogata formuła skladników
+            </li>
+            <li
+              className="relative my-2 before:inline-block before:w-3 before:h-3 before:mr-4
+            before:bg-green-400 before:rounded-full before:border before:border-green-600"
+            >
+              Potwierdzona skuteczność niezliczoną ilością badań laboratoryjnych
+            </li>
+            <li
+              className="relative my-2 before:inline-block before:w-3 before:h-3 before:mr-4
+            before:bg-green-400 before:rounded-full before:border before:border-green-600 after:inline-block"
+            >
+              Źródło wielu składników odżywczych
+            </li>
+          </ul>
+          <p></p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AfluxInfo;
