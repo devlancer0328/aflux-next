a15e1eed0859d3065d481dadf917498cce202fc8
devlancer0328 <topdev0328@proton.me>
2024-03-01 10:49:52 +0100

    add Gallery

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,11 +1,34 @@
 import React from "react";
+import PhotoAlbum, { Photo, PhotoAlbumProps } from "react-photo-album";
+import product_img from "@/images/products/product01.jpg";
 
-const Gallery = () => {
+interface PhotoType {
+  src: string;
+  width: number;
+  height: number;
+}
+
+const photos: PhotoType[] = [
+  { src: `${product_img.src}`, width: 400, height: 500 },
+  { src: `${product_img.src}`, width: 400, height: 500 },
+  { src: `${product_img.src}`, width: 400, height: 500 },
+  { src: `${product_img.src}`, width: 400, height: 500 },
+  { src: `${product_img.src}`, width: 400, height: 500 },
+  { src: `${product_img.src}`, width: 400, height: 500 },
+  { src: `${product_img.src}`, width: 400, height: 500 },
+  { src: `${product_img.src}`, width: 400, height: 500 },
+];
+
+const Gallery: React.FC = () => {
+  const albumProps: PhotoAlbumProps<PhotoType> = {
+    layout: "masonry",
+    photos: photos,
+  };
+
   return (
-    <div className="flex bg-white w-full justify-center" id="gallery">
-      <div className="flex flex-row container mx-xl py-32 justify-center items-center">
-        <h1 className="text-5xl mb-4">Galeria</h1>
-      </div>
+    <div className="p-10 bg-white">
+      <h1 className="text-5xl mt-8 mb-16 text-center">Galeria</h1>
+      <PhotoAlbum {...albumProps} />
     </div>
   );
 };
