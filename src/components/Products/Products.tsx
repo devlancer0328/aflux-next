bb959451484c3d4381c138e8816d07be345e31da
devlancer0328 <topdev0328@proton.me>
2024-03-01 17:43:50 +0100

    fill & styling Products component

--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import product_img from "@/images/products/product01.jpg";
 
+import "@/components/Products/products.scss";
+
 const Products = () => {
   const ProductsItemsList = [
     {
@@ -9,6 +11,8 @@ const Products = () => {
       height: 341,
       text: "Aflux krem",
       alt_text: "produkt",
+      text_description:
+        "Aflux, renomowana marka kosmetyków, prezentuje swoją najnowszą propozycję - krem do rąk o niezrównanej jakości i skuteczności. Ten wyjątkowy produkt został stworzony z myślą o pielęgnacji skóry dłoni, dbając jednocześnie o nawilżenie, odżywienie i ochronę. Formuła kremu została opracowana z użyciem najwyższej jakości składników, które doskonale pielęgnują i regenerują skórę. Bogate w substancje odżywcze i witaminy, krem głęboko nawilża skórę, przywracając jej elastyczność i miękkość. Dzięki zawartości naturalnych ekstraktów roślinnych, zapewnia on intensywną regenerację nawet najbardziej wysuszonej skóry.",
     },
     {
       source: product_img.src,
@@ -16,20 +20,28 @@ const Products = () => {
       height: 341,
       text: "Aflux krem",
       alt_text: "produkt",
+      text_description:
+        "Aflux, renomowana marka kosmetyków, prezentuje swoją najnowszą propozycję - krem do rąk o niezrównanej jakości i skuteczności. Ten wyjątkowy produkt został stworzony z myślą o pielęgnacji skóry dłoni, dbając jednocześnie o nawilżenie, odżywienie i ochronę. Formuła kremu została opracowana z użyciem najwyższej jakości składników, które doskonale pielęgnują i regenerują skórę. Bogate w substancje odżywcze i witaminy, krem głęboko nawilża skórę, przywracając jej elastyczność i miękkość. Dzięki zawartości naturalnych ekstraktów roślinnych, zapewnia on intensywną regenerację nawet najbardziej wysuszonej skóry.",
     },
+
     {
       source: product_img.src,
       width: 300,
       height: 341,
       text: "Aflux krem",
       alt_text: "produkt",
+      text_description:
+        "Aflux, renomowana marka kosmetyków, prezentuje swoją najnowszą propozycję - krem do rąk o niezrównanej jakości i skuteczności. Ten wyjątkowy produkt został stworzony z myślą o pielęgnacji skóry dłoni, dbając jednocześnie o nawilżenie, odżywienie i ochronę. Formuła kremu została opracowana z użyciem najwyższej jakości składników, które doskonale pielęgnują i regenerują skórę. Bogate w substancje odżywcze i witaminy, krem głęboko nawilża skórę, przywracając jej elastyczność i miękkość. Dzięki zawartości naturalnych ekstraktów roślinnych, zapewnia on intensywną regenerację nawet najbardziej wysuszonej skóry.",
     },
+
     {
       source: product_img.src,
       width: 300,
       height: 341,
       text: "Aflux krem",
       alt_text: "produkt",
+      text_description:
+        "Aflux, renomowana marka kosmetyków, prezentuje swoją najnowszą propozycję - krem do rąk o niezrównanej jakości i skuteczności. Ten wyjątkowy produkt został stworzony z myślą o pielęgnacji skóry dłoni, dbając jednocześnie o nawilżenie, odżywienie i ochronę. Formuła kremu została opracowana z użyciem najwyższej jakości składników, które doskonale pielęgnują i regenerują skórę. Bogate w substancje odżywcze i witaminy, krem głęboko nawilża skórę, przywracając jej elastyczność i miękkość. Dzięki zawartości naturalnych ekstraktów roślinnych, zapewnia on intensywną regenerację nawet najbardziej wysuszonej skóry.",
     },
   ];
 
@@ -38,18 +50,28 @@ const Products = () => {
       <div className="flex flex-col container py-32 justify-center items-center">
         <h1 className="text-5xl mb-4">Produkty</h1>
         <div
-          className="flex flex-row flex-wrap gap-10 justify-center items-center"
+          className="products-wrapper flex flex-col flex-wrap gap-10 justify-center items-center"
           data-aos="fade-up"
         >
           {ProductsItemsList.map((ProductsListItem) => (
-            <div className="flex flex-col justify-center items-center my-16">
-              <img
-                src={ProductsListItem.source}
-                alt={ProductsListItem.alt_text}
-                width={ProductsListItem.width}
-                height={ProductsListItem.height}
-              />
-              <p>{ProductsListItem.text}</p>
+            <div className="products-wrapper__slot flex flex-row justify-center items-center my-16">
+              <div className="flex flex-col">
+                <img
+                  className="me-10"
+                  src={ProductsListItem.source}
+                  alt={ProductsListItem.alt_text}
+                  width={ProductsListItem.width}
+                  height={ProductsListItem.height}
+                />
+              </div>
+              <div className="flex flex-col">
+                <p className="text-xl mb-4 font-bold">
+                  {ProductsListItem.text}
+                </p>
+                <p className="text-sm w-[30vw]">
+                  {ProductsListItem.text_description}
+                </p>
+              </div>
             </div>
           ))}
         </div>
