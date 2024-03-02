f5c162cd549846ab08a3934d0469a836c901da7e
devlancer0328 <topdev0328@proton.me>
2024-03-02 18:42:56 +0100

    styling & fill footer

--- src/components/common/Footer/Footer.tsx
+++ src/components/common/Footer/Footer.tsx
@@ -1,10 +1,45 @@
 import React from "react";
+import logo from "@/images/aflux-logo.png";
+import { HEADER_LINKS } from "@/constants/header-links";
+import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div className="flex bg-white w-full justify-center">
-      <div className="flex flex-row container mx-xl py-4 justify-center items-center">
-        <p className="text-slg m-4">Aflux © 2024</p>
+    <div className="flex bg-green-600 w-full justify-center">
+      <div className="flex flex-col container mx-xl lg:py-4 justify-center items-center">
+        <div className="flex flex-col lg:flex-row items-center justify-between w-full">
+          <div className="mt-6 lg:mt-0">
+            <a className="inline-flex bg-white p-2 mx-4 rounded-lg" href="/">
+              <img src={logo.src} alt="logo" width={48} />
+            </a>
+          </div>
+          <div>
+            <ul className="flex flex-wrap m-4 text-center flex-row items-center justify-between">
+              {HEADER_LINKS.map((link) => (
+                <Link
+                  className="p-4 text-sm text-white hover:text-black"
+                  href={link.href}
+                  key={link.key}
+                >
+                  {link.label}
+                </Link>
+              ))}
+            </ul>
+          </div>
+          <div></div>
+        </div>
+        <div className="flex flex-col lg:flex-row items-center justify-between w-full">
+          <p className="text-sm m-4 text-white">Aflux © 2024</p>
+          <p className="text-sm m-4 text-white">
+            Coded with ❤️ by{" "}
+            <a
+              className="hover:text-black"
+              href="https://goldipl.github.io/Marcin-Programuje/"
+            >
+              goldipl
+            </a>
+          </p>
+        </div>
       </div>
     </div>
   );
