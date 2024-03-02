9591702a4568b074bb8969c74a6ba560fe4ee9e9
devlancer0328 <topdev0328@proton.me>
2024-03-02 19:38:16 +0100

    styling contact iframe  with using clip-path

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 
+import "@/components/Contact/contact.scss";
+
 const Contact = () => {
   return (
     <div className="flex bg-green-50 w-full justify-center" id="contact">
       <div className="flex flex-col container mx-xl pt-16 pb-0 lg:py-24 justify-center items-center">
         <h1 className="text-3xl lg:text-5xl mb-16">Kontakt</h1>
-        <div className="flex flex-col lg:flex-row justify-between items-center w-full">
-          <div className="flex flex-col border-4 border-white w-full lg:w-1/2 me-0 mb-6 lg:me-8 lg:mb-0 h-full rounded-2xl p-6 items-center justify-center shadow-inner">
+        <div className="relative flex flex-col lg:flex-row justify-between items-center w-full">
+          <div className="flex flex-col border-4 border-white w-full me-0 mb-6 lg:me-8 lg:mb-0 h-[200px] rounded-2xl px-4 lg:px-12 items-start justify-center shadow-inner">
             <p className="mb-2">
               <strong className="me-4 font-extrabold">E-mail:</strong>
               <a
@@ -28,7 +30,7 @@ const Contact = () => {
           </div>
           <div className="flex flex-col">
             <iframe
-              className="w-[100vw] lg:w-[720px] relative lg:rounded-2xl border-2 border-white shadow-inner"
+              className="absolute top-0 right-0 w-[40vw] lg:w-[720px] lg:rounded-2xl border-2 border-white shadow-inner"
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d156388.35438479282!2d20.92111239761172!3d52.23306532094908!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471ecc669a869f01%3A0x72f0be2a88ead3fc!2sWarszawa!5e0!3m2!1spl!2spl!4v1668164739561!5m2!1spl!2spl"
               width="800"
               height="200"
