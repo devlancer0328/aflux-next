a3ba42e9ba975ed0d64a81eb043a794859d7065e
devlancer0328 <topdev0328@proton.me>
2024-03-02 17:24:04 +0100

    change iframe width to full

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -25,7 +25,7 @@ const Contact = () => {
           </div>
           <div className="flex flex-col">
             <iframe
-              className="relative rounded-2xl border-2 border-green-200"
+              className="w-full relative rounded-2xl border-2 border-green-200"
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d156388.35438479282!2d20.92111239761172!3d52.23306532094908!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471ecc669a869f01%3A0x72f0be2a88ead3fc!2sWarszawa!5e0!3m2!1spl!2spl!4v1668164739561!5m2!1spl!2spl"
               width="800"
               height="200"
